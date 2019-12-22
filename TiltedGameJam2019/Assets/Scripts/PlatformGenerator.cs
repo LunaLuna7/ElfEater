d1beb43440fd69ff56c759e0bc4751fab046a37c
michacg <michacg2@uci.edu>
2019-12-21 17:59:11 -0800

    procedural works but no constraint doing it rn thanks

--- TiltedGameJam2019/Assets/Scripts/PlatformGenerator.cs
+++ TiltedGameJam2019/Assets/Scripts/PlatformGenerator.cs
@@ -31,13 +31,31 @@ public class PlatformGenerator : MonoBehaviour
         float ySpawnValue = Camera.main.transform.position.y + Random.Range( Camera.main.pixelHeight, 2 * Camera.main.pixelHeight);
         Instantiate(platform, new Vector2(xSpawnValue, ySpawnValue), Quaternion.identity);
 
-        for (int num = 0; num < platformLen; num++)
+        int num = 0;
+        while (num < platformLen) 
         {
-            int blockIndex = Random.Range(-1, blocks.Count);
-            float xBlock = xSpawnValue + (blocks[0].GetComponent<SpriteRenderer>().bounds.size.x) * (num);
+            int blockIndex;
+            if (num > 5)
+                blockIndex = Random.Range(-1, blocks.Count - (3 * (platformLen - num) ));
+            else
+                blockIndex = Random.Range(-1, blocks.Count);
+
+            int len = (int) Mathf.Ceil((float) (blockIndex+1) / 3);
+            print(len);
 
             if (blockIndex != -1)
+            {
+                float xBlock = (xSpawnValue + (blocks[0].GetComponent<SpriteRenderer>().bounds.size.x) * (num)) + blocks[blockIndex].GetComponent<SpriteRenderer>().bounds.size.x / 2;
+
                 Instantiate(blocks[blockIndex], new Vector2(xBlock, ySpawnValue), Quaternion.identity);
+                num += len;
+            }
+            else
+            {
+                num++;
+            }
+
+                
         }
 
     }
