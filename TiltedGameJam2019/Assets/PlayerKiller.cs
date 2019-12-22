0de7b6e599e9b0a7615d360c4859dd7f86b0afcf
Luis <lfluna@uci.edu>
2019-12-21 20:21:37 -0800

    Player Killer

---
+++ TiltedGameJam2019/Assets/PlayerKiller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKiller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update() 
+    {
+        rb.velocity = new Vector3(0, -speed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            Debug.Log("EndGame");
+        }    
+    }
+}
